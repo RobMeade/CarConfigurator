e072b35bd587dcdf8933a0072b25d0722fb9f029
Rob <baldy.meade@gmail.com>
2023-01-31 16:26:43 +0000

    Initial Project Commit

---
+++ Source/CarConfigurator/CarConfigurator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CarConfigurator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CarConfigurator, "CarConfigurator" );
---
+++ Source/CarConfigurator/CarConfiguratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CarConfiguratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARCONFIGURATOR_API ACarConfiguratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
